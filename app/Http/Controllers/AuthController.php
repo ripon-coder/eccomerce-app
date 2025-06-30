6d8495006d67cfbb94f96c88d1698f72aeee9ab0
coder-ripon <coderripon@gmail.com>
2025-06-30 14:26:36 +0600

    add basic login & register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        User::create($request->all());
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('multi-app-token')->plainTextToken;
+
+            return response()->json(['token' => $token, 'user' => $user]);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+}
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,18 +2,21 @@
 @section('content')
     <div class="row justify-content-center mt-4">
         <div class="card p-4" style="width: 40rem;">
-            <form action="{{route('login')}}" method="POST">
+            <form action="{{ route('register') }}" method="POST">
                 @csrf
+                <div class="form-group mt-2">
+                    <label>Name</label>
+                    <input type="text" class="form-control" name="name">
+                </div>
                 <div class="form-group mt-2">
                     <label>Email address</label>
                     <input type="email" class="form-control" name="email">
-                    <small class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group mt-2">
                     <label>Password</label>
                     <input type="password" class="form-control" name="password">
                 </div>
-                <button type="submit" class="btn btn-primary mt-3">Login</button>
+                <button type="submit" class="btn btn-primary mt-3">Register</button>
             </form>
         </div>
     </div>
